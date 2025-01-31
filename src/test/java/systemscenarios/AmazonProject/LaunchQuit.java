190e26047bd124a369f7237443d6840bde0ba40b
sameergirani <sameergirani85@gmail.com>
2025-01-31 14:46:47 +0530

    Class Amazon Project1

--- src/test/java/systemscenarios/AmazonProject/LaunchQuit.java
+++ src/test/java/systemscenarios/AmazonProject/LaunchQuit.java
@@ -5,6 +5,7 @@
 package systemscenarios.AmazonProject;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -16,7 +17,8 @@ public class LaunchQuit
 	@BeforeMethod
 	public void launch()
 	{
-		driver=new EdgeDriver();
+		//driver=new EdgeDriver();
+		driver=new ChromeDriver();
 		driver.get("https://www.amazon.in/");
 		driver.manage().window().maximize();
 	}
